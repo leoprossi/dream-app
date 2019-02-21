6fbfac4861c4ee55ab11870ffa4caa8cd9850747
Leonardo Rossi <leonardorossi.lpr@gmail.com>
2019-02-21 11:14:17 -0300

    Showing a confirmation alert when trying to delete alarm

--- src/shared/components/popover/popover.ts
+++ src/shared/components/popover/popover.ts
@@ -1,20 +1,45 @@
-import { Component } from '@angular/core';
-import { ViewController } from 'ionic-angular';
+import { Component, EventEmitter } from '@angular/core';
+import { ViewController, AlertController, NavController } from 'ionic-angular';
+import { CrudService } from '../../services/crud-service';
+import { AlarmsPage } from '../../../pages/alarms/alarm-list/alarm-list';
 
 @Component({
   selector: 'popover',
   templateUrl: 'popover.html'
 })
-export class PopoverComponent {
+export class PopoverComponent<T> {
 
-  item: any;
-  
+  item: T;
+  service: CrudService<T>
 
-  constructor(public viewCtrl: ViewController) {
+  constructor(public viewCtrl: ViewController, public alertCtrl: AlertController, public navCtrl: NavController) {
   }
 
   delete() {
+    this.presentConfirm();
     this.viewCtrl.dismiss();
   }
 
+  presentConfirm() {
+    let alert = this.alertCtrl.create({
+      title: 'Delete Warning',
+      message: 'Do you really want to delete this alarm?',
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          handler: () => {
+          }
+        },
+        {
+          text: 'Delete',
+          handler: () => {
+            this.service.delete(this.item).then(res => this.navCtrl.popTo(AlarmsPage));
+          },
+          cssClass: "danger"
+        }
+      ]
+    });
+    alert.present();
+  }
 }
